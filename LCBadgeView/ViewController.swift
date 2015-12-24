c0d74e2bae56b385681b28be46bbb3d44eb548ef
Cai Linfeng <cailinfeng1234@126.com>
2015-12-24 20:21:08 +0800

    Initial Commit

---
+++ LCBadgeView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LCBadgeView
+//
+//  Created by Cai Linfeng on 12/24/15.
+//  Copyright © 2015 GavinFlying. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
