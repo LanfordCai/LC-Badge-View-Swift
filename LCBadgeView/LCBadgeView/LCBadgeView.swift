c605801f0fb6ac8600ffcdbf4fdfa0039946a9b2
Cai Linfeng <cailinfeng1234@126.com>
2015-12-25 19:17:45 +0800

    Implemented basic features

---
+++ LCBadgeView/LCBadgeView/LCBadgeView.swift
@@ -0,0 +1,210 @@
+//
+//  LCBadgeView.swift
+//  LCBadgeView
+//
+//  Created by Cai Linfeng on 12/24/15.
+//  Copyright © 2015 GavinFlying. All rights reserved.
+//
+
+import UIKit
+
+
+class LCBadgeView: UIView {
+
+    enum BadgeShiftDirectionType {
+        case Left
+        case Justify
+        case Right
+    }
+
+    var text: String? {
+        didSet {
+            textLayer?.string = text
+            hideBadgeWhenZero()
+        }
+    }
+
+    var textColor: UIColor = UIColor.whiteColor() {
+        didSet {
+            textLayer?.foregroundColor = textColor.CGColor
+        }
+    }
+
+    var font: UIFont = UIFont.systemFontOfSize(14) {
+
+        didSet {
+            if font.lineHeight > frame.height {
+                print("Font lineheight larger than badgeView's height")
+                font = oldValue
+            }
+
+            textLayer?.font = font.fontName
+            textLayer?.fontSize = font.pointSize
+        }
+    }
+
+    var paddingFactor: CGFloat = 0.4 {
+        didSet {
+            setupBadgeFrame()
+        }
+    }
+
+    var cornerRadius: CGFloat? {
+        didSet {
+            badgePath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius ?? bounds.height * 0.5)
+            backgroundLayer?.path = badgePath!.CGPath
+            borderLayer?.path = badgePath!.CGPath
+        }
+    }
+
+    var shiftDirection: BadgeShiftDirectionType = .Left
+
+    var badgeBackgroundColor: UIColor = UIColor.redColor() {
+        didSet {
+            backgroundLayer?.fillColor = badgeBackgroundColor.CGColor
+        }
+    }
+
+    var borderColor: UIColor = UIColor.blackColor() {
+        didSet {
+            borderLayer?.strokeColor = borderColor.CGColor
+        }
+    }
+
+    var borderWidth: CGFloat = 0.0 {
+        didSet {
+            borderLayer?.lineWidth = borderWidth
+        }
+    }
+
+    var hideWhenZero = true
+    var maxWidth: CGFloat = CGFloat(FLT_MAX) {
+        didSet {
+            setupBadgeFrame()
+        }
+    }
+
+    private var textLayer: CATextLayer?
+    private var backgroundLayer: CAShapeLayer?
+    private var borderLayer: CAShapeLayer?
+    private var badgePath: UIBezierPath?
+
+    private var minWidth: CGFloat = 24.0
+
+    private var anchorLeftX: CGFloat?
+    private var anchorMiddleX: CGFloat?
+    private var anchorRightX: CGFloat?
+    private var defaultFontLineHeight: CGFloat = 17.0
+
+
+    // MARK: Life-Cycle Methods
+
+    init(frame: CGRect, shiftDirection: BadgeShiftDirectionType, cornerRadius: CGFloat) {
+        self.shiftDirection = shiftDirection
+        super.init(frame: frame)
+        configurations()
+        self.cornerRadius = cornerRadius
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configurations()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        print("layoutSubviews")
+        setupBadgeFrame()
+    }
+
+
+    // MARK: Private Methods
+
+    private func configurations() {
+        backgroundColor = UIColor.clearColor()
+        userInteractionEnabled = false
+        clipsToBounds = false
+
+        // Create textLayer
+        textLayer = CATextLayer()
+        textLayer?.foregroundColor = textColor.CGColor
+        textLayer?.font = font.fontName
+        textLayer?.fontSize = font.pointSize ?? 14.0
+        textLayer?.alignmentMode = kCAAlignmentCenter
+
+        // Create backgroundLayer
+        backgroundLayer = CAShapeLayer()
+
+        // Create boarderLyaer 
+        borderLayer = CAShapeLayer()
+        borderLayer?.strokeColor = borderColor.CGColor
+        borderLayer?.fillColor = UIColor.clearColor().CGColor
+        borderLayer?.lineWidth = borderWidth
+
+        layer.addSublayer(backgroundLayer!)
+        layer.addSublayer(borderLayer!)
+        layer.addSublayer(textLayer!)
+    }
+
+    private func setupBadgeFrame() {
+
+        anchorLeftX = anchorLeftX ?? frame.origin.x
+        anchorMiddleX = anchorMiddleX ?? frame.origin.x + 0.5 * bounds.width
+        anchorRightX = anchorRightX ?? frame.origin.x + bounds.width
+
+        minWidth = bounds.height != 0 ? bounds.height : 24.0
+
+        var tempFrame = frame
+        let textWidth = sizeForText(text).width
+
+        if textWidth < minWidth {
+            tempFrame.size.width = minWidth
+        } else if textWidth > maxWidth {
+            tempFrame.size.width = maxWidth
+        } else {
+            tempFrame.size.width = textWidth
+        }
+
+        switch shiftDirection {
+        case .Left:
+            tempFrame.origin.x = anchorRightX! - tempFrame.size.width
+        case .Justify:
+            tempFrame.origin.x = anchorMiddleX! - tempFrame.size.width * 0.5
+        case .Right:
+            break
+        }
+
+        frame = tempFrame
+
+
+        let textFrame = CGRect(x: 0, y: (bounds.height - font.lineHeight) * 0.5, width: bounds.width, height: font.lineHeight)
+        textLayer?.frame = textFrame
+
+
+        badgePath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius ?? bounds.height * 0.5)
+        backgroundLayer?.frame = bounds
+        backgroundLayer?.fillColor = badgeBackgroundColor.CGColor
+        backgroundLayer?.path = badgePath!.CGPath
+
+        borderLayer?.frame = bounds
+        borderLayer?.path = badgePath!.CGPath
+    }
+
+    private func sizeForText(text: String?) -> CGSize {
+        guard let text = text as NSString? else  {
+            return CGSize(width: 0.0, height: 0.0)
+        }
+
+        let widthPadding: CGFloat = font.pointSize * paddingFactor
+        let attrDict = [NSFontAttributeName: font]
+        var textSize = text.sizeWithAttributes(attrDict)
+
+        textSize.width += widthPadding * 2.0
+
+        return textSize
+    }
+
+    private func hideBadgeWhenZero() {
+        self.hidden = text == "0" && hideWhenZero
+    }
+}
--- LCBadgeView/ViewController.swift
+++ LCBadgeView/ViewController.swift
@@ -10,16 +10,129 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testView: UIView!
+    @IBOutlet weak var textField: UITextField!
+
+    var badgeView: LCBadgeView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        badgeView = LCBadgeView(frame: CGRectMake(0, 0, 24, 24), shiftDirection: .Left, cornerRadius: 24 * 0.5)
+        badgeView.center = CGPoint(x: testView.bounds.width, y: 0)
+        badgeView.text = "1"
+        testView.addSubview(badgeView)
+
+        badgeView.font = UIFont.systemFontOfSize(15)
+        badgeView.shiftDirection = .Left
+        badgeView.textColor = UIColor.whiteColor()
+        badgeView.badgeBackgroundColor = UIColor.redColor()
+        badgeView.borderColor = UIColor.whiteColor()
+        badgeView.borderWidth = 0.0
+        badgeView.maxWidth = 100.0
+        badgeView.paddingFactor = 0.5
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Font 有问题
+    @IBAction func fontChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            badgeView.font = UIFont.systemFontOfSize(15)
+        case 1:
+            badgeView.font = UIFont.systemFontOfSize(20)
+        default:
+            badgeView.font = UIFont.boldSystemFontOfSize(15)
+        }
+    }
+
+    @IBAction func cornerRadiusChanged(sender: UISlider) {
+        badgeView.cornerRadius = badgeView.bounds.height * CGFloat(sender.value)
+    }
+
+    @IBAction func shiftDirectionChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            badgeView.shiftDirection = .Left
+        case 1:
+            badgeView.shiftDirection = .Justify
+        default:
+            badgeView.shiftDirection = .Right
+        }
+    }
+
+    @IBAction func textColorChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            badgeView.textColor = UIColor.whiteColor()
+        case 1:
+            badgeView.textColor = UIColor.greenColor()
+        default:
+            badgeView.textColor = UIColor.yellowColor()
+        }
+    }
+
+    @IBAction func badgeBackgroundColorChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            badgeView.badgeBackgroundColor = UIColor.redColor()
+        case 1:
+            badgeView.badgeBackgroundColor = UIColor.blueColor()
+        default:
+            badgeView.badgeBackgroundColor = UIColor.blackColor()
+        }
+    }
+
+    @IBAction func borderColorChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            badgeView.borderColor = UIColor.whiteColor()
+        case 1:
+            badgeView.borderColor = UIColor.blackColor()
+        default:
+            badgeView.borderColor = UIColor.yellowColor()
+        }
+    }
+
+    @IBAction func hidesWhenZeroChanged(sender: UISwitch) {
+        badgeView.hideWhenZero = sender.on
+    }
+
+    @IBAction func maxWidthChanged(sender: UISlider) {
+        badgeView.maxWidth = CGFloat(sender.value)
+    }
+
+    @IBAction func borderWidthChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            badgeView.borderWidth = 0.0
+        case 1:
+            badgeView.borderWidth = 2.0
+        default:
+            badgeView.borderWidth = 4.0
+        }
+    }
+
+    @IBAction func paddingFactorChanged(sender: UISlider) {
+        badgeView.paddingFactor = CGFloat(sender.value + 0.5)
+    }
 
 }
+
+extension ViewController: UITextFieldDelegate {
+
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        badgeView.text = textField.text
+        return true
+    }
+
+    func textFieldDidBeginEditing(textField: UITextField) {
+        print("text \(textField.text)")
+    }
+}
+
 
